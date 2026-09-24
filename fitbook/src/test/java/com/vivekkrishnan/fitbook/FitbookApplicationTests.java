d35d0caae1af336fb0ac4eec9437cec61da1359f
vivekskrishnan <vivekskrishnan@gmail.com>
2026-09-24 14:57:38 -0700

    Springboot Initialization/Setup

---
+++ fitbook/src/test/java/com/vivekkrishnan/fitbook/FitbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekkrishnan.fitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
